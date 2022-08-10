0fc0d383655caceb922b1fdd64b4302a6fd0196f
Neyko641 <shendamere@gmail.com>
2022-08-10 16:25:04 +0300

    added two player and drew the background

--- main.cpp
+++ main.cpp
@@ -18,18 +18,17 @@ struct {
                 {800, 600},
                 {1280, 720}
 };
-enum inputs {
-    KEY_PRESS_DEFAULT,
-    KEY_PRESS_UP,
-    KEY_PRESS_DOWN,
-    KEY_PRESS_PAUSE,
+struct player_pos {
+    int x;
+    int y;
 };
+
 
 bool init_sdl_win (SDL_Renderer *&renderer, SDL_Window *&win, int screen_width, int screen_height) {
     int render_flags, win_flags;
     render_flags = SDL_RENDERER_ACCELERATED;
     win_flags = 0;
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         printf("Couldn't init SDL", SDL_GetError());
         return false;
     }
@@ -73,7 +72,19 @@ void handle_input(bool *state) {
 
     }
 }
+SDL_Rect init_player(int init_start_x, int init_start_y, int texture_height, int texture_width) {
+    SDL_Rect player;
+    player.h = texture_height;
+    player.w = texture_width;
+    player.x = init_start_x;
+    player.y = init_start_y;
 
+    return player;
+}
+SDL_Rect draw_player(SDL_Renderer *&renderer, SDL_Rect player) {
+    SDL_RenderDrawRect(renderer, &player);
+    SDL_RenderFillRect(renderer, &player);
+}
 
 int main() {
     const SDL_MessageBoxButtonData buttons[] = {
@@ -120,13 +131,27 @@ int main() {
     window application;
     application.width = resolutions[button_id].width;
     application.height = resolutions[button_id].height;
+
     bool is_running =  init_sdl_win(application.renderer, application.win, application.width, application.height);
-    //handle input
-    inputs input = KEY_PRESS_DEFAULT;
+
+
+    const int player_1_start_x_pos = 20;
+    const int player_start_y_pos = application.height / 2;
+    const int player_height = 70;
+    const int player_width = 15;
+    const int player_2_start_x_pos = application.width - ((player_1_start_x_pos * 2));
+    printf("player 1 is at %d and player 2 is at %d", player_1_start_x_pos,player_2_start_x_pos);
+    SDL_Rect p1 = init_player(player_1_start_x_pos, player_start_y_pos, player_height, player_width);
+    SDL_Rect p2 = init_player(player_2_start_x_pos, player_start_y_pos, player_height, player_width);
+
+
     while(is_running) {
         handle_input(&is_running);
         SDL_RenderClear(application.renderer);
-        SDL_SetRenderDrawColor(application.renderer, 0,0,0,255);
+        SDL_SetRenderDrawColor(application.renderer, 255,255,255,255);
+        draw_player(application.renderer, p1);
+        draw_player(application.renderer, p2);
+        SDL_SetRenderDrawColor(application.renderer, 0, 0, 0, 255);
         SDL_RenderPresent(application.renderer);
         //SDL_Delay(1000/application.FPS);
     }
